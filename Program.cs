23ae3c63777149f62ef0147b4ba74bd32913e812
Iryna <salagub.iruna@gmail.com>
2026-03-05 21:09:16 +0200

    Додано клас GraduateStudent, створено конструктор та метод PerformDuties

--- Program.cs
+++ Program.cs
@@ -54,4 +54,15 @@ public class UndergraduateStudent : UniversityMember
     }
 }
 
+public class GraduateStudent : UndergraduateStudent
+{
+    public GraduateStudent(string name, string id ) : base(name, id) {}
+
+    public override void PerformDuties()
+    {
+        base.PerformDuties();
+        ActionLog.Add("Thesis research update");
+    }
+}
+
 
